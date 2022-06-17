e2ba00087c9696bc85081791e41001bb3c4c86b1
MoonMoon82 <41437842+MoonMoon82@users.noreply.github.com>
2022-06-17 17:53:05 +0200

    Update ATinyEncoder.h

--- ATinyEncoder_lib/ATinyEncoder.h
+++ ATinyEncoder_lib/ATinyEncoder.h
@@ -1,38 +1,39 @@
 #include <Arduino.h>
 
+//Data structure to receive
 struct Info{
-  int8_t rotationCounter;
+  int8_t rotation;
   bool pushed;
 };
 
-class ATinyEnocder {
+
+//A simple class for easy usage of the ATinyEncoder
+class ATinyEncoder {
     private:
-    HardwareSerial* SerialPort;
-    boolean called = false;
-    int available=0;
+        HardwareSerial* SerialPort; //Target Serial port to communicate with the ATTiny
+        boolean called = false;     //Remember if a request was sent
 
     public:
-    Info RecPacket;
-    ATinyEnocder (HardwareSerial* targetPort){
-        SerialPort = targetPort;
-        SerialPort->begin(9600);
-    }
+        Info status;             //Status of the Encoder 
+        ATinyEncoder (HardwareSerial* targetPort){ //Initialize ATinyEncoder with Target Serial port
+            SerialPort = targetPort;
+            SerialPort->begin(9600);    //idk 9600baut works quite ok for me
+        }
+
     boolean handle(){
-        char ok = 90;
+        status.rotation = 0;        //reset saved rotation
         if ( called == false ){
-            SerialPort->write(ok);
-            called = true;
+            char RequestByte = 90;              //request byte 80 (idk should I chose another byte?)
+            SerialPort->write(RequestByte);     //send request to the ATTiny
+            called = true;                      //remember that the request was sent
         }
-        available = SerialPort->available();
-        if (available >= 2) {
-            SerialPort->readBytes((uint8_t*)(&RecPacket),2);
+
+        //if there are more than 2 bytes available in the serial port buffer, write it into the status structure
+        if (SerialPort->available() >= 2) {
+            SerialPort->readBytes((uint8_t*)(&status),2);
             if ( Serial1.available() == 0 ) { called = false; }
-            return true;
+            return true;    //return true to show that new data has arrived
         }
-    return false;
-    }
-    void reset(){
-        RecPacket.rotationCounter = 0;
+    return false; //return false to show that no new data was received
     }
 };
-
